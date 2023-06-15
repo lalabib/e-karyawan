40b1398459fa69358c1859606a080dd892c48802
lalabib <labibla21@gmail.com>
2023-06-15 17:57:31 +0700

    initial commit

---
+++ app/src/main/java/com/latihan/lalabib/e_karyawan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.latihan.lalabib.e_karyawan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
